38c2084358c783bf43caf7a8636b4cf4625b2563
Veselin Stefanov <veski4a92@gmail.com>
2020-09-10 22:12:57 +0300

    Initial Commit

---
+++ TemperatureConvertor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TemperatureConvertor
+//
+//  Created by Veselin Stefanov on 10.09.20.
+//  Copyright © 2020 Veselin Stefanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
